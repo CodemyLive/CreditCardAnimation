c2479f0fac7cca383f140915fc906e0f9f0ccbd8
Gökhan ÖZTÜRK <GokhanOZTURK@AndroidEdu.IO>
2020-03-12 22:58:28 +0300

    CreditCardAnimation added

--- creditcardanimation/src/main/java/live/codemy/creditcardanimation/CreditCardAnimation.kt
+++ creditcardanimation/src/main/java/live/codemy/creditcardanimation/CreditCardAnimation.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.cardview.widget.CardView
+import kotlinx.android.synthetic.main.view_credit_card.view.*
+import kotlinx.android.synthetic.main.view_credit_card_back.view.*
+import kotlinx.android.synthetic.main.view_credit_card_front.view.*
 
 class CreditCardAnimation @JvmOverloads constructor(
     context: Context,
@@ -15,6 +18,30 @@ class CreditCardAnimation @JvmOverloads constructor(
     defStyleAttr
 ) {
     init {
-        LayoutInflater.from(context).inflate(R.layout.view_credit_card_animation, this)
+        LayoutInflater.from(context).inflate(R.layout.view_credit_card, this)
+
+        btnCVC.setOnClickListener {
+            incCreditCardFront.animate()
+                .translationY(-(incCreditCardFront as CardView).height.toFloat())
+                .alpha(0.0f)
+                .duration = 1000
+
+            incCreditCardBack.animate()
+                .translationY((incCreditCardBack as CardView).height.toFloat())
+                .alpha(1.0f)
+                .duration = 1000
+        }
+
+        btnFront.setOnClickListener {
+            incCreditCardFront.animate()
+                .translationY(0f)
+                .alpha(1.0f)
+                .duration = 1000
+
+            incCreditCardBack.animate()
+                .translationY(0f)
+                .alpha(0.0f)
+                .duration = 1000
+        }
     }
 }
